25b3a2cf38da8e2c442f14d32f0cdcc998123b33
umxe <u_habiba@rocketmail.com>
2018-06-26 14:07:17 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,10 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! "
+    "Welcome to your app!!!! Oh my goodness, wow"
   end
 
+  get "/info" do
+    "Sinatra is cool"
+  end
 end
